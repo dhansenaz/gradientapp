a4c97df3212c702a75f9ace6111f67a154395f16
dhansenaz <dhansen@allthingsdot.com>
2019-02-07 19:08:50 -0800

    controller

--- server/controller.js
+++ server/controller.js
@@ -1,32 +1,24 @@
-
 let gradients = [
-    {
-        id:1,
-        category:'purple',
-        title:'midnight',
-        style:'background-image: linear-gradient(to top, #30cfd0 0%, #330867 100%);',
-        button:'get code'
-
-    },
-    {
-        id:2,
-        category:'blue',
-        title:'skyline',
-        style:'background-image: linear-gradient(to top, #48c6ef 0%, #6f86d6 100%);',
-        button:'get code'
-
-    }
-
-
-
-
-]
+  {
+    id: 1,
+    category: "purple",
+    title: "midnight",
+    style:
+      "background-image: linear-gradient(to top, #30cfd0 0%, #330867 100%);",
+    button: "get code"
+  },
+  {
+    id: 2,
+    category: "blue",
+    title: "skyline",
+    style:
+      "background-image: linear-gradient(to top, #48c6ef 0%, #6f86d6 100%);",
+    button: "get code"
+  }
+];
 
 module.exports = {
-
-    read: (req, res) => {
-        res.status(200).send(gradients)
-
-
-}
-}
+  read: (req, res) => {
+    res.status(200).send(gradients);
+  }
+};
