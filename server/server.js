65bbce2d96457edcdb78895345274cedde2d9e59
dhansenaz <dhansen@allthingsdot.com>
2019-03-23 09:04:02 -0700

    server cleanup

--- server/server.js
+++ server/server.js
@@ -1,12 +1,12 @@
-const express = require('express')
-const bodyParser = require('body-parser')
+const express = require("express");
+const bodyParser = require("body-parser");
 const PORT = 4000;
-const ctrl = require('./controller')
+const ctrl = require("./controller");
 
 const app = express();
 
 app.use(bodyParser.json());
 
-app.get('/api/gradients', ctrl.read)
+app.get("/api/gradients", ctrl.read);
 
-app.listen(PORT, () => console.log("You are running on port 4000"))
+app.listen(PORT, () => console.log("You are running on port 4000"));
