4c91da929f7ca2ee3f36461c7eed50afa7b1b461
dhansenaz <dhansen@allthingsdot.com>
2019-03-27 18:45:45 -0700

    css cleanup

--- src/components/SeeCodeButton.js
+++ src/components/SeeCodeButton.js
@@ -2,58 +2,54 @@ import React, { Component } from "react";
 import "../components/codebutton.css";
 
 let buttonStyle = {
-  backgroundColor:'green'
-}
+  backgroundColor: "green"
+};
 export default class SeeCodeButton extends Component {
   constructor() {
     super();
     this.state = {
       show: false,
-      code:'',
-      
+      code: ""
     };
     this.showCode = this.showCode.bind(this);
     this.copyCode = this.copyCode.bind(this);
-    
   }
   showCode() {
-    this.setState({ show: true, code:'background-image:' + this.props.gradient.style +';' });
+    this.setState({
+      show: true,
+      code: "background-image:" + this.props.gradient.style + ";"
+    });
   }
   copyCode() {
-    const el = document.createElement('textarea');
+    const el = document.createElement("textarea");
     el.value = this.state.code;
-    el.setAttribute('readonly', '');
-    el.style.position = 'absolute';
-    el.style.left = '-9999px';
+    el.setAttribute("readonly", "");
+    el.style.position = "absolute";
+    el.style.left = "-9999px";
     document.body.appendChild(el);
     el.select();
-    document.execCommand('copy');
+    document.execCommand("copy");
     document.body.removeChild(el);
-    
+
     alert("Copied to Clipboard!!");
-    
-    
-    
-   
   }
-  
+
   render() {
-    let display = <div className='fadein'>
-     {this.state.code};
-     <button onClick={this.copyCode} className="code-button">
+    let display = (
+      <div className="fadein">
+        {this.state.code};
+        <button onClick={this.copyCode} className="code-button">
           COPY CODE
         </button>
-      </div>;
+      </div>
+    );
     if (!this.state.show) {
       display = (
-        <button onClick={this.showCode } className="code-button">
+        <button onClick={this.showCode} className="code-button">
           SEE CODE
         </button>
-        
       );
     }
     return <div className="css-code">{display}</div>;
   }
 }
-
-
