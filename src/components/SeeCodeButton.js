5907d5fdb9965c997a846296277dd050f60faee1
dhansenaz <dhansen@allthingsdot.com>
2019-03-27 17:12:16 -0700

    added copy code functionality

--- src/components/SeeCodeButton.js
+++ src/components/SeeCodeButton.js
@@ -5,22 +5,43 @@ export default class SeeCodeButton extends Component {
   constructor() {
     super();
     this.state = {
-      show: false
+      show: false,
+      code:''
     };
     this.showCode = this.showCode.bind(this);
+    this.copyCode = this.copyCode.bind(this);
+    
   }
   showCode() {
-    this.setState({ show: true });
+    this.setState({ show: true, code:'background-image:' + this.props.gradient.style +';' });
+  }
+  copyCode() {
+    const el = document.createElement('textarea');
+    el.value = this.state.code;
+    el.setAttribute('readonly', '');
+    el.style.position = 'absolute';
+    el.style.left = '-9999px';
+    document.body.appendChild(el);
+    el.select();
+    document.execCommand('copy');
+    document.body.removeChild(el);
   }
   render() {
-    let display = <div>background-image:{this.props.gradient.style};</div>;
+    let display = <div className='fadein'>
+     {this.state.code};
+     <button onClick={this.copyCode} className="code-button">
+          COPY CODE
+        </button>
+      </div>;
     if (!this.state.show) {
       display = (
         <button onClick={this.showCode} className="code-button">
           SEE CODE
         </button>
+        
       );
     }
     return <div className="css-code">{display}</div>;
   }
 }
+
