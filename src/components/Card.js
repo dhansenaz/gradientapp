4c1f64dc386b984c0244e63f57293e55c95aeb45
dhansenaz <dhansen@allthingsdot.com>
2019-04-14 09:10:30 -0700

    cardjs cleanup

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,20 +2,19 @@ import React, { Component } from "react";
 import "./card.css";
 import SeeCodeButton from "../components/SeeCodeButton";
 
-const Card = ({gradients}) => {
-    let cards = gradients.map(element => {
-      return (
-        <div className="card">
-          <div key={element.id}>
-            <h1>{element.title}</h1>
-          </div>
-          <div className="circle" style={{ backgroundImage: element.style }} />
-          <SeeCodeButton gradient={element} />
+const Card = ({ gradients }) => {
+  let cards = gradients.map(element => {
+    return (
+      <div className="card">
+        <div key={element.id}>
+          <h1>{element.title}</h1>
         </div>
-      );
-    });
-    return <div className="card-container">{cards}</div>;
-}
-
+        <div className="circle" style={{ backgroundImage: element.style }} />
+        <SeeCodeButton gradient={element} />
+      </div>
+    );
+  });
+  return <div className="card-container">{cards}</div>;
+};
 
 export default Card;
